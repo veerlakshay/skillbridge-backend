3a8744f2db99b81d3f2f51dc9e403284c11fbccb
veerlakshay <lakshay.rv@gmail.com>
2025-06-19 22:20:00 -0400

    Initial Spring Boot project setup

---
+++ backend/src/main/java/com/skillbridge/backend/SkillbridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillbridge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillbridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillbridgeBackendApplication.class, args);
+	}
+
+}
